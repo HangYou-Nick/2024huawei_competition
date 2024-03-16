fb9fe3d4153d7de1233d51386ee6048404052446
HangYou <30865989+HangYou-Nick@users.noreply.github.com>
2024-03-16 20:17:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import time
 n = 200
 robot_num = 10
 berth_num = 10
@@ -13,7 +14,7 @@ class Robot:
         self.mbx = mbx
         self.mby = mby
 
-robot = [Robot() for _ in range(robot_num + 10)]
+robot = [Robot() for _ in range(robot_num)]
 
 class Berth:
     def __init__(self, x=0, y=0, transport_time=0, loading_speed=0):
@@ -22,7 +23,7 @@ class Berth:
         self.transport_time = transport_time
         self.loading_speed = loading_speed
 
-berth = [Berth() for _ in range(berth_num + 10)]
+berth = [Berth() for _ in range(berth_num)]
 
 class Boat:
     def __init__(self, num=0, pos=0, status=0):
@@ -30,7 +31,7 @@ class Boat:
         self.pos = pos
         self.status = status
 
-boat = [Boat() for _ in range(10)]
+boat = [Boat() for _ in range(5)]
 
 
 money = 0
@@ -51,13 +52,36 @@ def Init():
         berth[id].y = berth_list[2]
         berth[id].transport_time = berth_list[3]
         berth[id].loading_speed = berth_list[4]
+    global boat_capacity
     boat_capacity = int(input())
     okk = input()
     print("OK")
     sys.stdout.flush()
+
+def offlineInit():
+    with open("maps/map1.txt", "r") as f:
+        content = f.readlines()
+    for i in range(0, n):
+        line = content[i].replace("\n", "")
+        ch.append([c for c in line.split(sep=" ")])
+    with open("maps/meta1.txt", "r") as f:
+        content = f.readlines()
+    for i in range(berth_num):
+        line = content[i].replace("\n", "")
+        berth_list = [int(c) for c in line.split(sep=" ")]
+        id = berth_list[0]
+        berth[id].x = berth_list[1]
+        berth[id].y = berth_list[2]
+        berth[id].transport_time = berth_list[3]
+        berth[id].loading_speed = berth_list[4]
+    global boat_capacity
+    boat_capacity = int(content[i+1])
+    sys.stdout.flush()
+
 def Input():
     id, money = map(int, input().split(" "))
     num = int(input())
+    global gds
     for i in range(num):
         x, y, val = map(int, input().split())
         gds[x][y] = val
@@ -68,10 +92,26 @@ def Input():
     okk = input()
     return id
 
+def offlineInput():
+    with open("maps/first_output1.txt", "r") as f:
+        id, money = map(int, f.readline().split(" "))
+        num = int(f.readline())
+        global gds
+        for i in range(num):
+            x, y, val = map(int, f.readline().split())
+            gds[x][y] = val
+        for i in range(robot_num):
+            robot[i].goods, robot[i].x, robot[i].y, robot[i].status = map(int, f.readline().split())
+        for i in range(5):
+            boat[i].status, boat[i].pos = map(int, f.readline().split())
+    return id
+
 if __name__ == "__main__":
-    Init()
+    # Init()
+    offlineInit()
     for zhen in range(1, 15001):
-        id = Input()
+        id = offlineInput()
+        # id = Input()
         for i in range(robot_num):
             print("move", i, random.randint(0, 3))
             sys.stdout.flush()
