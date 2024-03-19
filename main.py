c84785a642f75e8f8368247fb3cf11b3e7a5763c
CodeJackcl <chenl725915@163.com>
2024-03-19 14:59:33 +0800

    cl

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import random
 import time
+
 n = 200
 robot_num = 10
 berth_num = 10
 N = 210
+
+
 class Robot:
     def __init__(self, robot_idx, startX=0, startY=0, goods=0, status=0, mbx=0, mby=0):
         self.robot_idx = robot_idx
@@ -18,31 +21,22 @@ class Robot:
     # 机器人的移动行为
     def move(self, id, direction):
         if isinstance(id, int) and 0 <= id <= 9:
-            if direction == 0:
-                self.x = self.x
-                self.y = self.y + 1
-            if direction == 1:
-                self.x = self.x
-                self.y = self.y - 1
-            if direction == 2:
-                self.x = self.x - 1
-                self.y = self.y
-            if direction == 3:
-                self.x = self.x + 1
-                self.y = self.y
-        print("move", id, direction)
+            print("move", id, direction)
 
     # 机器人获取物品的行为
     def get(self, id):
         if isinstance(id, int) and 0 <= id <= 9:
+            # self.status = 1
             print("get", id)
 
     # 机器人卸载物品的行为
     def pull(self, id):
         if isinstance(id, int) and 0 <= id <= 9:
+            # self.status = 0
             print("pull", id)
 
 robot = [Robot(i) for i in range(robot_num)]
+
 
 class Berth:
     def __init__(self, berth_idx, x=0, y=0, transport_time=0, loading_speed=0):
@@ -53,6 +47,7 @@ class Berth:
         self.loading_speed = loading_speed
 
 berth = [Berth(i) for i in range(berth_num)]
+
 
 class Boat:
     def __init__(self, boat_idx, num=0, pos=0, status=0):
@@ -74,12 +69,12 @@ class Boat:
 boat = [Boat(i) for i in range(5)]
 
 
-
 money = 0
 boat_capacity = 0
 id = 0
 ch = []
 gds = [[0 for _ in range(N)] for _ in range(N)]
+
 
 def Init():
     for i in range(0, n):
@@ -98,6 +93,7 @@ def Init():
     okk = input()
     print("OK")
     sys.stdout.flush()
+
 
 def offlineInit():
     with open("maps/map1.txt", "r") as f:
@@ -119,6 +115,7 @@ def offlineInit():
     boat_capacity = int(content[i+1])
     sys.stdout.flush()
 
+
 def Input():
     global id
     global money
@@ -134,6 +131,7 @@ def Input():
         boat[i].status, boat[i].pos = map(int, input().split())
     okk = input()
     return id
+
 
 def offlineInput():
     global id
@@ -151,15 +149,40 @@ def offlineInput():
             boat[i].status, boat[i].pos = map(int, f.readline().split())
     return id
 
+
+def getDirection():
+    a = 1
+
+
 if __name__ == "__main__":
-    Init()
-    # offlineInit()
+    #Init()
+    offlineInit()
+
+    sorted_berth = sorted(berth, key=lambda x: x.loading_speed, reverse=True) # 按照搬运速度进行降序排序
+    berth_pos = []
+    for i in range(5):
+        berth_pos.append([sorted_berth[i].x, sorted_berth[i].y])
+
+    # for i in range(len(berth)):
+    #     print(sorted_berth[i].x, sorted_berth[i].y, sorted_berth[i].transport_time, sorted_berth[i].loading_speed)
+
     for zhen in range(1, 15001):
-        # id = offlineInput()
-        id = Input()
+        id = offlineInput()
+        # id = Input()
+
+         # 货物的位置如何表示
+        goods_pos  = [(i, j) for i in range(len(gds)) for j in range(len(gds[i])) if gds[i][j] != 0]
+
+        directionList = getDirection() # y由算法得到每个机器人应该朝着什么方向前进
         for i in range(robot_num):
-            print("move", i, random.randint(0, 3))
-            sys.stdout.flush()
+            robot[i].move(i, directionList[i])
+            robot_pos = [robot[i].x, robot[i].y]
+            
+            if robot_pos in goods_pos:
+                robot[i].get(i)
+            if robot_pos in berth_pos:
+                robot[i].pull(i)
+
         for i in range(5):
             boat[i].ship(i)
             boat[i].go()
